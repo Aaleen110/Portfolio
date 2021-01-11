3a1789e3591b7bbaed6207af0b545a0bac2ed8b9
Developer2 <aaleen.mirza@copious.care>
2021-01-11 17:48:11 +0530

    Updates

---
+++ precache-manifest.c04382576d0221e3af3c4a4372efd697.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e005d895c4a7619d241845930cbf1e5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d8e6718c358063a241bb",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab5223f788d1df9a2d60",
+    "url": "/Portfolio/static/js/2.f9a9ee69.chunk.js"
+  },
+  {
+    "revision": "bf9c9f3949d3e79ea9f0fdb73b0cf200",
+    "url": "/Portfolio/static/js/2.f9a9ee69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e6718c358063a241bb",
+    "url": "/Portfolio/static/js/main.fb524497.chunk.js"
+  },
+  {
+    "revision": "2808ec3bca3a9cfb923d",
+    "url": "/Portfolio/static/js/runtime-main.dc08a4dc.js"
+  },
+  {
+    "revision": "4478bafd5aed781c27a28f1423407159",
+    "url": "/Portfolio/static/media/aaleen.4478bafd.png"
+  },
+  {
+    "revision": "79026dbecafa62b405e08224a9ce2d58",
+    "url": "/Portfolio/static/media/aziziya_app.79026dbe.webp"
+  },
+  {
+    "revision": "6ecc7b75c8dd56e09ed8023dfff82631",
+    "url": "/Portfolio/static/media/dribbble_icon.6ecc7b75.png"
+  },
+  {
+    "revision": "549f119d38c6c67e17e29f91b8759698",
+    "url": "/Portfolio/static/media/email_icon.549f119d.png"
+  },
+  {
+    "revision": "53f6bf4cb7a90d91817881856c81112a",
+    "url": "/Portfolio/static/media/fb_icon.53f6bf4c.png"
+  },
+  {
+    "revision": "2e86742d6b816a127e8d311e86bf7166",
+    "url": "/Portfolio/static/media/instagram_icon.2e86742d.webp"
+  },
+  {
+    "revision": "fbfee0f754968171d8731fe6f0427dda",
+    "url": "/Portfolio/static/media/javascript.fbfee0f7.png"
+  },
+  {
+    "revision": "cc39f2c3df2018a356170c548045a7f3",
+    "url": "/Portfolio/static/media/livehealth_app.cc39f2c3.webp"
+  },
+  {
+    "revision": "b647d873bda7dc441ca003b06fffc422",
+    "url": "/Portfolio/static/media/livehealthdoc_app.b647d873.webp"
+  },
+  {
+    "revision": "382e0b9eba9c1b0edc2dc70b6de8d453",
+    "url": "/Portfolio/static/media/livehealthlogo.382e0b9e.jpg"
+  },
+  {
+    "revision": "368473ce3ce291eeceef2bbf4587e9dc",
+    "url": "/Portfolio/static/media/passwordgenerator_app.368473ce.webp"
+  },
+  {
+    "revision": "e818622294ad0a0c4ebedf9ee8b8a008",
+    "url": "/Portfolio/static/media/quiz_app.e8186222.webp"
+  },
+  {
+    "revision": "cf8550ca41843035e2e7666ca360f0df",
+    "url": "/Portfolio/static/media/smartwomen_app.cf8550ca.webp"
+  },
+  {
+    "revision": "c388521a631ac97b9935989ee934afc3",
+    "url": "/Portfolio/static/media/twitter_icon.c388521a.webp"
+  }
+]);
